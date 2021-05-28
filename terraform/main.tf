98e947fbc08e0a938b3f9f38f2b19ed1054c533a
Luc <luc@lucemans.nl>
2021-05-28 23:50:46 +0200

    Deployment and Healthchecks

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,7 @@ provider "kubernetes" {
 variable "container" {}
 variable "deployurl" {}
 
-resource "kubernetes_pod" "auth" {
+resource "kubernetes_deployment" "auth" {
     metadata {
         name = "auth"
         namespace = "dogehouse"
@@ -25,74 +25,103 @@ resource "kubernetes_pod" "auth" {
     }
 
     spec {
-        container {
-            image = var.container
-            name = "auth"
-
-            env {
-                name = "GITHUB_CLIENT_SECRET"
-                value_from {
-                    config_map_key_ref {
-                        name = "config"
-                        key = "GITHUB_CLIENT_SECRET"
-                    }
-                }
-            }
-
-            env {
-                name = "GITHUB_CLIENT_ID"
-                value_from {
-                    config_map_key_ref {
-                        name = "config"
-                        key = "GITHUB_CLIENT_ID"
-                    }
-                }
-            }
-
-            env {
-                name = "AUTH_TOKEN"
-                value_from {
-                    config_map_key_ref {
-                        name = "config"
-                        key = "AUTH_TOKEN"
-                    }
-                }
-            }
-            env {
-                name = "POSTGRES_HOST"
-                value_from {
-                    config_map_key_ref {
-                        name = "config"
-                        key = "POSTGRES_HOST"
-                    }
-                }
-            }
-            env {
-                name = "POSTGRES_PASSWORD"
-                value_from {
-                    config_map_key_ref {
-                        name = "config"
-                        key = "POSTGRES_PASSWORD"
-                    }
-                }
-            }
-            env {
-                name = "GITHUB_REDIRECT_URI"
-                value_from {
-                    config_map_key_ref {
-                        name = "config"
-                        key = "GITHUB_REDIRECT_URI"
-                    }
-                }
-            }
-
-            port {
-                container_port = 3000
+        replicas = 3
+        selector {
+            match_labels = {
+                app = "auth"
             }
         }
-        
-        image_pull_secrets {
-            name = "regcred"
+    }
+
+    template {
+        metadata {
+            name = "auth"
+            namespace = "dogehouse"
+            labels = {
+                app = "auth"
+            }
+        }
+
+        spec {
+            container {
+                image = var.container
+                name = "auth"
+
+                env {
+                    name = "GITHUB_CLIENT_SECRET"
+                    value_from {
+                        config_map_key_ref {
+                            name = "config"
+                            key = "GITHUB_CLIENT_SECRET"
+                        }
+                    }
+                }
+
+                env {
+                    name = "GITHUB_CLIENT_ID"
+                    value_from {
+                        config_map_key_ref {
+                            name = "config"
+                            key = "GITHUB_CLIENT_ID"
+                        }
+                    }
+                }
+
+                env {
+                    name = "AUTH_TOKEN"
+                    value_from {
+                        config_map_key_ref {
+                            name = "config"
+                            key = "AUTH_TOKEN"
+                        }
+                    }
+                }
+                env {
+                    name = "POSTGRES_HOST"
+                    value_from {
+                        config_map_key_ref {
+                            name = "config"
+                            key = "POSTGRES_HOST"
+                        }
+                    }
+                }
+                env {
+                    name = "POSTGRES_PASSWORD"
+                    value_from {
+                        config_map_key_ref {
+                            name = "config"
+                            key = "POSTGRES_PASSWORD"
+                        }
+                    }
+                }
+                env {
+                    name = "GITHUB_REDIRECT_URI"
+                    value_from {
+                        config_map_key_ref {
+                            name = "config"
+                            key = "GITHUB_REDIRECT_URI"
+                        }
+                    }
+                }
+
+                port {
+                    container_port = 3000
+                }
+
+                liveness_probe {
+                    http_get {
+                        path = "/"
+                        port = 3000
+                    }
+
+                    initial_delay_seconds = 3
+                    period_seconds        = 3
+                }
+            }
+            
+            image_pull_secrets {
+                name = "regcred"
+            }
         }
     }
 }
