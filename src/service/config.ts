52f660d9dbeb9c636be05d15314ada311b2f4e62
Luc <luc@lucemans.nl>
2021-05-30 18:17:31 +0200

    Introduce Redirect URL

--- src/service/config.ts
+++ src/service/config.ts
@@ -1,5 +1,6 @@
 export const CONFIG = {
     GITHUB_URL: 'https://github.com/login/oauth/authorize',
     GOOGLE_URL: 'https://accounts.google.com/o/oauth2/auth',
-    DISCORD_URL: 'https://discord.com/api/oauth2/authorize'
+    DISCORD_URL: 'https://discord.com/api/oauth2/authorize',
+    REDIRECT_URL: 'https://dogehouse.online/dashboard'
 }
